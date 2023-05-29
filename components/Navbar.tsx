8bdab4ea5085eb6037edbbb6f17da3641dac3104
GrantCoding <gh.business10@gmail.com>
2023-05-28 20:22:57 -0500

    Reverted global css changes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -79,6 +79,7 @@ export default function Navbar () {
                     </Link>
                   )
                 })}
+                {currentTheme==="light"}
                 {/* {currentTheme === "dark" ? (
                   <button
                     onClick={() => setTheme("light")}
